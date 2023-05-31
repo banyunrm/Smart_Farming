605772dd1f3efe19005e2238615f4050764a3a61
ardelia123 <deliazzarin@gmail.com>
2023-05-31 11:58:33 +0700

    second comment

---
+++ lib/page/device_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DevicePage extends StatefulWidget {
+
+  @override
+  State<DevicePage> createState() => _DevicePageState();
+}
+
+class _DevicePageState extends State<DevicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/page/garden_page.dart
+++ lib/page/garden_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
 
 class GardenPage extends StatefulWidget {
   @override
@@ -6,7 +10,51 @@ class GardenPage extends StatefulWidget {
 }
 
 class _GardenPageState extends State<GardenPage> {
+  List<Map<String, dynamic>> farmList = [];
+  String _name = '';
+  String _jenis = '';
+  final _nameField = TextEditingController();
+  final _jenisField = TextEditingController();
+  var addData;
+
+  double longitude = 0.0;
+  double latitude = 0.0;
+  int farm = 0;
+  final _longitudeField = TextEditingController();
+  final _latitudeField = TextEditingController();
+  final _farmField = TextEditingController();
+
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void _storeData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    var newData = prefs.getString('farmdata');
+    List<Map<String, dynamic>> convertedData =
+        List<Map<String, dynamic>>.from(jsonDecode(newData ?? "[]"));
+    farmList = convertedData;
+
+    if (_nameField.text.isEmpty || _jenisField.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            duration: Duration(seconds: 2),
+            content: Text('Nama dan Jenis Tanaman tidak boleh kosong!')),
+      );
+      return;
+    }
+
+    addData = {
+      "name": _name,
+      "jenis": _jenis,
+      "longitude": longitude,
+      "latitude": latitude,
+      "farm": farm
+    };
+
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -63,18 +111,18 @@ class _GardenPageState extends State<GardenPage> {
                 child: Center(
                   child: Column(
                     children: [
-                      Container (
+                      Container(
                         margin: EdgeInsets.only(top: 20),
-                         child: Text(
-                        'Kebun',
-                        style: TextStyle(
-                          fontSize: 28.0,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w600,
-                          color: Color.fromARGB(255, 23, 107, 95),
+                        child: Text(
+                          'Tambah Lahan',
+                          style: TextStyle(
+                            fontSize: 28.0,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(255, 23, 107, 95),
+                          ),
                         ),
                       ),
-                        ),
                       Padding(
                         padding: EdgeInsets.only(top: 20, left: 15, right: 15),
                         child: TextField(
@@ -86,7 +134,7 @@ class _GardenPageState extends State<GardenPage> {
                           ),
                           autofocus: false,
                           decoration: InputDecoration(
-                            labelText: 'Nama Device',
+                            labelText: 'Nama Kebun',
                             contentPadding: EdgeInsets.all(1.0),
                             labelStyle: TextStyle(
                               fontSize: 16.0,
@@ -104,7 +152,7 @@ class _GardenPageState extends State<GardenPage> {
                         ),
                       ),
                       Padding(
-                        padding: EdgeInsets.only(left: 15, right: 15),
+                        padding: EdgeInsets.only(top: 20, left: 15, right: 15),
                         child: TextField(
                           style: TextStyle(
                             fontSize: 16.0,
@@ -114,7 +162,35 @@ class _GardenPageState extends State<GardenPage> {
                           ),
                           autofocus: false,
                           decoration: InputDecoration(
-                            labelText: 'Nama Device',
+                            labelText: 'Jenis Tanaman',
+                            contentPadding: EdgeInsets.all(1.0),
+                            labelStyle: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromARGB(255, 23, 107, 95),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+                        child: TextField(
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(255, 23, 107, 95),
+                          ),
+                          autofocus: false,
+                          decoration: InputDecoration(
+                            labelText: 'Luas Lahan',
                             contentPadding: EdgeInsets.all(1.0),
                             labelStyle: TextStyle(
                               fontSize: 16.0,
